e3d62e2d15f2b0bc35c6d3a60e6b8d785394b51d
mingcheng lin <insert github email here>
2019-08-15 14:55:50 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './App.css';
+import CounterOne from './Components/CounterOne';
+
+function App() {
+  return (
+    <div className="App">
+    <CounterOne/>
+    </div>
+  );
+}
+
+export default App;
