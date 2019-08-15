2a99a27e8b62780430f23806481140601a281e02
mingcheng lin <insert github email here>
2019-08-15 15:37:55 -0400

    add button

--- src/Components/CounterTwo.js
+++ src/Components/CounterTwo.js
@@ -1,7 +1,8 @@
 import React,{useReducer} from 'react'
 
 const initialState = {
-    firstCounter :0
+    firstCounter :0,
+    secondCounter: 10
 }
 
 
@@ -9,9 +10,13 @@ const initialState = {
 const reducer =(state,action)=>{
  switch(action.type){
      case 'increment':
-         return {firstCounter : state.firstCounter+1}
+         return {...state,firstCounter : state.firstCounter+ action.value}
      case 'decrement':
-         return {firstCounter : state.firstCounter-1}
+         return {...state,firstCounter : state.firstCounter- action.value}
+         case 'increment2':
+            return {...state,secondCounter : state.secondCounter+ action.value}
+        case 'decrement2':
+            return {...state,secondCounter : state.secondCounter- action.value}
      case 'reset':
          return initialState
      default:
@@ -27,10 +32,23 @@ function CounterTwo() {
     return (
 
         <div>
-            <h1>Count2:{count.firstCounter}</h1>
-            <button onClick={()=>dispatch({type: 'increment'})}>Increment</button>
-            <button onClick={()=>dispatch({type: 'decrement'})}>Decrement</button>
+            <h1>First Count:{count.firstCounter}</h1>
+             <h1>Second Count:{count.secondCounter}</h1>
+            <button onClick={()=>dispatch({type: 'increment' ,value:1 })}>Increment</button>
+            <button onClick={()=>dispatch({type: 'decrement' ,value:1 })}>Decrement</button>
+            <button onClick={()=>dispatch({type: 'increment' ,value:5 })}>Increment 5</button>
+            <button onClick={()=>dispatch({type: 'decrement' ,value:5 })}>Decrement 5</button>
             <button onClick={()=>dispatch({type: 'reset'})}>Reset</button>
+
+           <div>
+           <button onClick={()=>dispatch({type: 'increment2' ,value:1 })}>Increment2</button>
+            <button onClick={()=>dispatch({type: 'decrement2' ,value:1 })}>Decrement2</button>
+
+
+           </div>
+
+
+
         </div>
     )
 }
